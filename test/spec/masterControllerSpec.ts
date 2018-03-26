0ced2aecd9b09b2bd35a571e51da7c72d2faad60
Patrick Glavin <patjglavin@gmail.com>
2018-03-26 17:09:37 -0400

    begin build tests on master controller

--- test/spec/masterControllerSpec.ts
+++ test/spec/masterControllerSpec.ts
@@ -5,7 +5,7 @@ describe('In the file masterController.ts', () => {
     describe('the masterController\'s', () => {
         let sut: vdogg.MasterController,
             $rootScope: ng.IRootScopeService, 
-            $controller: ng.IComponentControllerService,
+            $controller: ng.IControllerService,
             eventNames: vdogg.EventNames,
             masterControllerParams: {
                 $rootScope: ng.IRootScopeService;
@@ -24,7 +24,50 @@ describe('In the file masterController.ts', () => {
             sut = $controller<vdogg.MasterController>('masterController', masterControllerParams);
         });
         describe('constructor', () => {
-            
+            it('should set familiarName to Dillon', () => {
+                expect(sut.familiarName).toEqual('Dillon');
+            });
+            it('should set speciesName to homo Sapiens', () => {
+                expect(sut.speciesName).toEqual('Homo Sapiens');
+            });
+            it('should add 2 items to masterActions', () => {
+                expect(sut.masterActions.length).toEqual(2);
+            });
+            it('should set first item actionName in masterActions to \'Throw Object\'', () => {
+                expect(sut.masterActions[0].actionName).toEqual('Throw Object');
+            });
+            it('should set the first item actionFunc in masterActions', () => {
+                expect(sut.masterActions[0].actionFunc).toBeDefined('actionFunc not defined for Throw Object');
+                expect(sut.masterActions[0].actionFunc).not.toBeNull('actionFunc is null for Throw Object');
+            })
+            it('should set second item actionName in masterActions to \'Feed\'', () => {
+                expect(sut.masterActions[1].actionName).toEqual('Feed');
+            });
+            it('should set the second item actionFunc in masterActions', () => {
+                expect(sut.masterActions[1].actionFunc).toBeDefined('actionFunc not defined for Feed');
+                expect(sut.masterActions[1].actionFunc).not.toBeNull('actionFunc is null for Feed');
+            });
+            it('should add 6 items to masterObjects', () => {
+                expect(sut.masterObjects.length).toEqual(6);
+            });
+            it('should set masterObject item 1 name to \'Babe Ruth autographed baseball\'', () => {
+                expect(sut.masterObjects[0].name).toEqual('Babe Ruth autographed baseball');
+            });
+            it('should set masterObject item 2 name to \'ball\'', () => {
+                expect(sut.masterObjects[1].name).toEqual('ball');
+            });
+            it('should set masterObject item 3 name to \'Frisbee\'', () => {
+                expect(sut.masterObjects[2].name).toEqual('Frisbee');
+            });
+            it('should set masterObject item 4 name to \'stick\'', () => {
+                expect(sut.masterObjects[3].name).toEqual('stick');
+            });
+            it('should set masterObject item 5 name to \'dog food\'', () => {
+                expect(sut.masterObjects[4].name).toEqual('dog food');
+            });
+            it('should set masterObject item 6 name to \'table scraps\'', () => {
+                expect(sut.masterObjects[5].name).toEqual('table scraps');
+            });
         });
         describe('throwSomething method', () => {
             
