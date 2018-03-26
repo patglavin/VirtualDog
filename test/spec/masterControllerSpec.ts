ba10df9c4939454d0786a010db4fdad2189da203
Patrick Glavin <patjglavin@gmail.com>
2018-03-26 16:19:16 -0400

    possibly wrong start branch

--- test/spec/masterControllerSpec.ts
+++ test/spec/masterControllerSpec.ts
@@ -1,3 +1,39 @@
 /// <reference path="../../typings/index.d.ts" />
+import vdogg = dogsrus.virtdog;
 
-describe('In the file masterController.ts')
+describe('In the file masterController.ts', () => {
+    describe('the masterController\'s', () => {
+        let sut: vdogg.MasterController,
+            $rootScope: ng.IRootScopeService, 
+            $controller: ng.IComponentControllerService,
+            eventNames: vdogg.EventNames,
+            masterControllerParams: {
+                $rootScope: ng.IRootScopeService;
+                eventNames: vdogg.EventNames
+            };
+        beforeEach(() => {
+            angular.mock.module('app.people');
+            inject(($injector: ng.auto.IInjectorService) => {
+                $controller = $injector.get<ng.IControllerService>('$controller');
+                $rootScope = $injector.get<ng.IRootScopeService>('$rootScope');
+                masterControllerParams = {
+                    $rootScope: $rootScope,
+                    eventNames: vdogg.eventNames
+                };
+            });
+            sut = $controller<vdogg.MasterController>('masterController', masterControllerParams);
+        });
+        describe('constructor', () => {
+            
+        });
+        describe('throwSomething method', () => {
+            
+        });
+        describe('feedTheDog method', () => {
+            
+        });
+    });
+    describe('the MasterAction object\'s constructor', () => {
+        
+    });
+})
